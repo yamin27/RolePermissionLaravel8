bd497a18bc0d6d705f2e9a3746514d40eecdd3d9
Yamin Alam Sarker <yaminalam27@gmail.com>
2020-10-21 19:28:07 +0600

    Role & Permission Done

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+    Route::group(['middleware' => ['auth']], function() {
+        Route::resource('roles', \App\Http\Controllers\RoleController::class);
+        Route::resource('users', \App\Http\Controllers\UserController::class);
+        Route::resource('products', \App\Http\Controllers\ProductController::class);
+    });
